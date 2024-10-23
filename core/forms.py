e5688f4424772789cde1b731a741f9254b4dc069
vibitha <vibithavijayan6@gmail.com>
2024-10-23 12:41:58 +0530

    edit and delete of car

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,4 @@
-import re
+
 from django import forms
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth.models import User
@@ -44,7 +44,7 @@ class SignupForm(UserCreationForm):
     # Custom validation for password
     def clean_password1(self):
         password = self.cleaned_data.get('password1')
-        if len(password) < 8:
+        if len(password) < 5:
             raise forms.ValidationError("Password must be at least 8 characters long.")
         
         # Check for at least one special character
--- item/forms.py
+++ item/forms.py
@@ -64,3 +64,58 @@ class NewItemForm(forms.ModelForm):
                 'style': 'width: 100%; height: 20px;'
             }),
         }
+
+
+
+class EditItemForm(forms.ModelForm):
+    
+    class Meta:
+        model = Item
+        fields = ( 'name', 'description', 'price', 'image', 'company', 'model', 'seats', 'fuel_type', 'gear_type','is_sold')
+        
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'placeholder': 'Enter Item Name',
+                'class': INPUT_CLASSES,
+                'style': 'width: 100%; height: 20px;'
+            }),
+            'company': forms.TextInput(attrs={
+                'placeholder': 'Enter Item Company',
+                'class': INPUT_CLASSES,
+                'style': 'width: 100%; height: 20px;'
+            }),
+            'model': forms.TextInput(attrs={
+                'placeholder': 'Enter Item Model',
+                'class': INPUT_CLASSES,
+                'style': 'width: 100%; height: 20px;'
+            }),
+            'seats': forms.TextInput(attrs={
+                'placeholder': 'Enter Number of Seats',
+                'class': INPUT_CLASSES,
+                'style': 'width: 100%; height: 20px;'
+            }),
+            'fuel_type': forms.TextInput(attrs={
+                'placeholder': 'Enter Item Fuel Type',
+                'class': INPUT_CLASSES,
+                'style': 'width: 100%; height: 20px;'
+            }),
+            'gear_type': forms.TextInput(attrs={
+                'placeholder': 'Enter Item Gear Type',
+                'class': INPUT_CLASSES,
+                'style': 'width: 100%; height: 20px;'
+            }),
+            'description': forms.Textarea(attrs={
+                'placeholder': 'Enter Item Description',
+                'class': INPUT_CLASSES,
+                'style': 'width: 100%; height: 100px;'  # Increasing height for description
+            }),
+            'price': forms.TextInput(attrs={
+                'placeholder': 'Enter Item Price',
+                'class': INPUT_CLASSES,
+                'style': 'width: 100%; height: 20px;'
+            }),
+            'image': forms.FileInput(attrs={
+                'class': INPUT_CLASSES,
+                'style': 'width: 100%; height: 20px;'
+            }),
+        }
